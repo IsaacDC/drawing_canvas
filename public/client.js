b16000f874dd70fe525026527b001c52cbecb7dc
Isaac Del Castillo <52974036+IsaacDC@users.noreply.github.com>
2024-05-08 14:04:43 -0400

    Load previous drawings WIP

--- public/client.js
+++ public/client.js
@@ -13,11 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let isDrawing = false;
   let lastMouseX = 0;
   let lastMouseY = 0;
+  let color;
 
-  // Handle mouse events using event delegation
+  // mouse events
   canvas.addEventListener("mousedown", startDrawing);
   canvas.addEventListener("mousemove", draw);
   canvas.addEventListener("mouseup", stopDrawing);
+
+  
 
   function startDrawing(e) {
     isDrawing = true;
@@ -41,7 +44,27 @@ document.addEventListener("DOMContentLoaded", () => {
     socket.emit("stopDrawing");
   }
 
-  // Handle incoming socket events
+  // incoming socket events
+  socket.on('loadDrawingData', (data) => {
+    data.forEach(event => {
+      if (event.type === 'start') {
+        ctx.beginPath();
+        ctx.moveTo(event.x, event.y);
+        isDrawing = true;
+        lastMouseX = event.x;
+        lastMouseY = event.y;
+      } else if (event.type === 'draw') {
+        ctx.lineTo(event.x, event.y);
+        ctx.stroke();
+        lastMouseX = event.x;
+        lastMouseY = event.y;
+      } else if (event.type === 'stop') {
+        isDrawing = false;
+        ctx.beginPath();
+      }
+    });
+  });
+
   socket.on("startDrawing", ({ x, y }) => {
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -57,5 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
     isDrawing = false;
     ctx.beginPath();
   });
+
   resizeCanvas();
+
 });
--- server.js
+++ server.js
@@ -2,31 +2,37 @@ const express = require("express");
 const app = express();
 var server = app.listen(3000);
 app.use(express.static('public'));
-
 console.log("Socket is running");
 
 var socket = require('socket.io');
 var io = socket(server);
 
+var drawingData = [];
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Handle start drawing event
+  socket.emit('loadDrawingData', drawingData);
+
+  // start drawing event
   socket.on('startDrawing', ({ x, y }) => {
+    drawingData.push({ type: 'start', x, y });
     socket.broadcast.emit('startDrawing', { x, y });
   });
 
-  // Handle drawing event
+  // drawing event
   socket.on('draw', ({ x, y }) => {
+    drawingData.push({ type: 'draw', x, y });
     socket.broadcast.emit('draw', { x, y });
   });
 
-  // Handle stop drawing event
+  // stop drawing event
   socket.on('stopDrawing', () => {
+    drawingData.push({ type: 'stop' });
     socket.broadcast.emit('stopDrawing');
   });
 
-  // Handle disconnect event
+  // disconnect event
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
